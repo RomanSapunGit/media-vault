0da7478de59c3c056bf3f8aca2ced1d20ad7c3be
RomanSapunGit <sapunroma23@gmail.com>
2025-05-22 16:44:47 +0200

    feat(media): add main page view, update url in media_vault
    Added index view which will render main page and also send users count as context. Also added path to media app

---
+++ media/views.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+
+@login_required()
+def index(request: HttpRequest) -> HttpResponse:
+    media_users_count = get_user_model().objects.count()
+    context = {
+        "media_users": media_users_count
+    }
+    return render(request, "home/index.html", context)
--- media_vault/urls.py
+++ media_vault/urls.py
@@ -1,8 +1,10 @@
 
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path("", include("media.urls", namespace="media")),
+    path("auth/", include("authentication.urls", namespace="authentication")),
     path("admin/", admin.site.urls),
 ] + debug_toolbar_urls()
