4c69489648fa644e921437ac0f5a629bf4a301c7
RomanSapunGit <sapunroma23@gmail.com>
2025-05-23 13:01:17 +0200

    refactor(register): add auto-login
    Added auto-login after successful registration and redirect to main page

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,9 @@
-from django.contrib.auth import authenticate
+
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.views import LoginView
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from authentication.forms import SignUpForm, MediaUserLoginForm
 from media.models import MediaUser
@@ -17,8 +20,9 @@ def register_user(request):
             username = form.cleaned_data.get("username")
             raw_password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_password)
+            login(request, user)
             if user:
-                return render(request,"home/index.html")
+                return HttpResponseRedirect(reverse("media:index"))
         else:
             msg = "Form is not valid"
     else:
@@ -31,5 +35,6 @@ def register_user(request):
 
 class UserLoginView(LoginView):
     model = MediaUser
+    redirect_authenticated_user = True
     template_name = "account/login.html"
     form_class = MediaUserLoginForm
