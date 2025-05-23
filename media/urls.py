918102cac5ee35c50b36f36de55a5bb09cdf9422
RomanSapunGit <sapunroma23@gmail.com>
2025-05-23 16:36:17 +0200

    feat(GenreView): add genre view
    Added genre view with path to it, in view was used session in which title name is stored and customized get_context_data, which additionally sets as a context redirect_url and title for better user experience
    (redirect_url for redirecting users to title based on genre name they've chosen, title for changing button name and redirect_url).

--- media/urls.py
+++ media/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from media.views import (index,)
+from media.views import (index, GenreListView, )
 
 urlpatterns = [
     path("", index, name="index"),
+    path("genres/", GenreListView.as_view(), name="genre_list"),
+    path("books/", GenreListView.as_view(), name="book_list"),
+    path("comics/", GenreListView.as_view(), name="comic_list"),
+    path("series/", GenreListView.as_view(), name="series_list"),
+    path("films/", GenreListView.as_view(), name="film_list"),
+    path("anime/", GenreListView.as_view(), name="anime_list")
 ]
 
 app_name = "media"
--- media/views.py
+++ media/views.py
@@ -2,6 +2,13 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse
+from django.template.defaultfilters import title
+from django.utils.http import urlencode
+from django.views import generic
+
+from media.forms import GenreSearchForm
+from media.models import Genre
 
 
 @login_required()
@@ -11,3 +18,34 @@ def index(request: HttpRequest) -> HttpResponse:
         "media_users_count": media_users_count
     }
     return render(request, "home/index.html", context)
+
+class GenreListView(generic.ListView):
+    model = Genre
+    template_name = "media/genre_list.html"
+    paginate_by = 10
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context["search_form"] = GenreSearchForm(
+            initial={"title": title}
+        )
+
+        titles_dict = {
+            "book": reverse("media:book_list"),
+            "film": reverse("media:film_list"),
+            "comic": reverse("media:comic_list"),
+            "series": reverse("media:series_list"),
+            "anime": reverse("media:anime_list")
+        }
+
+        if "title" in self.request.GET and self.request.GET["title"] in titles_dict:
+            title_name = self.request.GET["title"]
+            self.request.session["genre_title"] = title_name
+        elif "genre_title" in self.request.session.keys() and self.request.GET["title"] in titles_dict:
+            title_name = self.request.session["genre_title"]
+        else:
+            title_name = "book"
+
+        context["redirect_url"] = titles_dict[title_name]
+        context["title"] = title_name
+        return context
