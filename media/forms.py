8e66ed3642d9a4a7f8ee61a55cb9bec3c7790aba
RomanSapunGit <sapunroma23@gmail.com>
2025-05-24 20:45:56 +0200

    fix(search): add access to empty field
    Added option to type nothing so that it's possible to search through all possible entries without changing query params/change pages so search data will be reset

--- media/forms.py
+++ media/forms.py
@@ -4,6 +4,7 @@ from django import forms
 class GenreSearchForm(forms.Form):
     name = forms.CharField(
         max_length=255,
+        required=False,
         widget=forms.TextInput(attrs={
             'placeholder': 'Search name...',
             'type': 'text',
