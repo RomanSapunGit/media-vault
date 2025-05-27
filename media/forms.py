96a975d7209954c931a574f305fc212c19d62a11
RomanSapunGit <sapunroma23@gmail.com>
2025-05-27 16:23:06 +0200

    style(media forms): change quotes
    Updated single quotes to double ones

--- media/forms.py
+++ media/forms.py
@@ -5,8 +5,23 @@ class GenreSearchForm(forms.Form):
     name = forms.CharField(
         max_length=255,
         required=False,
-        widget=forms.TextInput(attrs={
-            'placeholder': 'Search name...',
-            'type': 'text',
-        }),
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search name...",
+                "type": "text",
+            }
+        ),
+    )
+
+
+class MediaSearchForm(forms.Form):
+    title = forms.CharField(
+        max_length=255,
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search title...",
+                "type": "text",
+            }
+        ),
     )
