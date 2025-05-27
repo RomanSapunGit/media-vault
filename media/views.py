12fa05329b3ab8142ad2e33a36132b3e11e4ae6d
RomanSapunGit <sapunroma23@gmail.com>
2025-05-27 17:07:46 +0200

    [200~feat(book list): add book list view
    Added view which handles pagination (10 per page), filtering (currently by type choice (Manga, Traditional book etc.), title of the book, genres (if book has at least one from the list))
    also prefetches creators and genres to avoid N + 1 problem. Also added to context quey params for better retrieval in templates (also in genre list).

--- media/views.py
+++ media/views.py
@@ -1,12 +1,19 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Prefetch
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.urls import reverse
 from django.views import generic
 
-from media.forms import GenreSearchForm
-from media.models import Genre
+from media.forms import (
+    GenreSearchForm,
+    MediaSearchForm,
+    GenreFilterForm
+)
+from media.models import Genre, Book
+from media.utils import get_reverse_choice
 
 
 @login_required()
@@ -17,7 +24,8 @@ def index(request: HttpRequest) -> HttpResponse:
     }
     return render(request, "home/index.html", context)
 
-class GenreListView(generic.ListView):
+
+class GenreListView(LoginRequiredMixin, generic.ListView):
     model = Genre
     template_name = "media/genre-list.html"
     paginate_by = 10
@@ -45,6 +53,7 @@ class GenreListView(generic.ListView):
 
         context["redirect_url"] = media_dict[media_name]
         context["media"] = media_name
+        context["query_params"] = self.request.GET.copy()
         return context
 
     def get_queryset(self):
@@ -53,5 +62,48 @@ class GenreListView(generic.ListView):
         if form.is_valid():
             return queryset.filter(
                 name__icontains=form.cleaned_data["name"]
+            )
+        return queryset
+
+
+class BookListView(LoginRequiredMixin, generic.ListView):
+    model = Book
+    paginate_by = 10
+    template_name = "media/book-list.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context["query_params"] = self.request.GET.copy()
+        context["type_choices"] = [
+            choice_type[1]
+            for choice_type in Book.type.field.choices
+        ]
+        if "genres" in self.request.GET:
+            context["genre_filter_form"] = GenreFilterForm(self.request.GET)
+        else:
+            context["genre_filter_form"] = GenreFilterForm()
+        context["search_form"] = MediaSearchForm(self.request.GET)
+        return context
+
+    def get_queryset(self):
+        queryset = Book.objects.all().prefetch_related("creators", "genres")
+        search_form = MediaSearchForm(self.request.GET)
+
+        if search_form.is_valid():
+            queryset = queryset.filter(
+                title__icontains=search_form.cleaned_data["title"]
+            )
+
+        type_choice = self.request.GET.get("type")
+        db_stored_choice = get_reverse_choice(type_choice, Book.type)
+        if db_stored_choice:
+            queryset = queryset.filter(
+                type=db_stored_choice
+            )
+
+        genres_form = GenreFilterForm(self.request.GET)
+        if genres_form.is_valid():
+            queryset = queryset.filter(
+                genres__name__in=genres_form.cleaned_data["genres"]
             )
         return queryset
