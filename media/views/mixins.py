89c60675418f36e3926351da35e86970e9a9f388
RomanSapunGit <sapunroma23@gmail.com>
2025-06-09 16:19:22 +0200

    feat(query param binding): add genre and type binding

--- media/views/mixins.py
+++ media/views/mixins.py
@@ -9,6 +9,14 @@ class BookMutateMixin:
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        db_type = get_reverse_choice(self.request.GET.get("type", None), Book.type)
+        genre_ids = []
+        if "genres" in self.request.GET:
+            genre_ids = Genre.objects.filter(
+                name__in=self.request.GET.getlist("genres")
+            ).values_list("id", flat=True)
+        query_param_dict = {"type": db_type, "genres": genre_ids}
+
         context["media_name"] = "book"
         return context
 
