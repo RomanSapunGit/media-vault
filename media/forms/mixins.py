a092bd87f8db97a9b9697277f280a9df1a3a1b55
RomanSapunGit <sapunroma23@gmail.com>
2025-06-13 16:04:04 +0200

    refactor(media form mixin): add media form mixin constants

--- media/forms/mixins.py
+++ media/forms/mixins.py
@@ -1,9 +1,25 @@
 from django import forms
 
-from media.models import Creator, Genre, Media
+from media.models import Creator, Genre
 
 
 class MediaFormMixin(forms.ModelForm):
+    BASE_FIELDS = (
+        "title",
+        "description",
+        "created_at",
+        "creators",
+        "genres",
+    )
+
+    BASE_WIDGETS = {
+        "title": forms.TextInput(attrs={"class": "form-control"}),
+        "description": forms.Textarea(attrs={"class": "form-control"}),
+        "created_at": forms.DateInput(
+            attrs={"class": "form-control", "type": "date"}
+        )
+    }
+
     creators = forms.ModelMultipleChoiceField(
         queryset=Creator.objects.all(),
         required=False,
