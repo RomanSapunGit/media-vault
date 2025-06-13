03db3437976349ce775c433a2a0712678d5f9c0f
RomanSapunGit <sapunroma23@gmail.com>
2025-06-13 16:06:47 +0200

    fix(models): add required imports

--- media/models.py
+++ media/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import (MinValueValidator, MaxValueValidator,
+                                    MinLengthValidator, MaxLengthValidator)
 from django.db import models
 from django.db.models import UniqueConstraint
 
