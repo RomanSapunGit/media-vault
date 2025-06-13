87f3b007dd55a01d05f8f0cb734b30af339d4dc4
RomanSapunGit <sapunroma23@gmail.com>
2025-06-13 16:01:02 +0200

    feat(series form): add series form

--- media/forms/forms.py
+++ media/forms/forms.py
@@ -81,3 +81,28 @@ class FilmForm(MediaFormMixin):
             "country": forms.TextInput(attrs={"class": "form-control"}),
             "duration": forms.TimeInput(attrs={"class": "form-control"}),
         }
+
+
+class SeriesForm(MediaFormMixin):
+    class Meta:
+        model = Series
+        fields = MediaFormMixin.BASE_FIELDS + (
+            "country",
+            "status",
+            "seasons",
+            "series_number",
+            "type"
+        )
+
+        widgets = {
+            **MediaFormMixin.BASE_WIDGETS,
+            "country": forms.TextInput(attrs={"class": "form-control"}),
+            "status": forms.Select(attrs={"class": "form-control"}),
+            "seasons": forms.NumberInput(attrs={"class": "form-control"}),
+            "series_number": forms.NumberInput(
+                attrs={"class": "form-control"}
+            ),
+            "type": forms.Select(attrs={"class": "form-control"}),
+        }
+
+
