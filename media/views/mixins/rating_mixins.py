fd238b5ee9465060419045f8aa111f04aafc2bd1
RomanSapunGit <sapunroma23@gmail.com>
2025-06-23 19:04:07 +0200

    feat(rating view mixin): add rating view mixin

---
+++ media/views/mixins/rating_mixins.py
@@ -0,0 +1,18 @@
+from media.forms.forms import UserMediaRatingForm
+from media.models import UserMediaRating
+
+
+class RatingViewMixin:
+    model = UserMediaRating
+    form_class = UserMediaRatingForm
+    template_name = "media/form/form.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["media_name"] = "rating"
+        return context
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update({"user": self.request.user})
+        return kwargs
