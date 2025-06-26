711e4fdf5f77a8c7c6d1a4b351ed7347613c89c8
RomanSapunGit <sapunroma23@gmail.com>
2025-06-26 17:06:53 +0200

    feat(creator form): add creator form

--- media/forms/forms.py
+++ media/forms/forms.py
@@ -7,4 +7,33 @@ from media.models import (
 )
 
 
+class CreatorForm(CustomErrorMessageMixin, forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+        }
+        )
+    )
+    middle_name = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+        }
+        )
+    )
+    last_name = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+        }
+        )
+    )
+    birth_date = forms.DateField(
+        widget=forms.DateInput(attrs={
+            "class": "form-control", "type": "date"
+        }
+        )
+    )
 
+    class Meta:
+        model = Creator
+        fields = "__all__"
