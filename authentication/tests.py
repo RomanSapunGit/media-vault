4b39c4312f4588564445a7eb5bce9ff5f98b9f0a
RomanSapunGit <sapunroma23@gmail.com>
2025-08-08 17:53:50 +0200

    test(register): add test cases for register view
    Added verifications that in case of correct form redirect to different view, otherwise show errors.

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class RegisterViewTests(TestCase):
+    def test_user_register_valid(self):
+        response = self.client.post(
+            reverse("authentication:register"),
+            data={
+                "username": "user1",
+                "email": "user@user.com",
+                "password1": "%amgf83501767",
+                "password2": "%amgf83501767"
+            }
+        )
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_user_register_invalid(self):
+        response = self.client.post(
+            reverse("authentication:register"),
+            data={
+                "username": "user1",
+                "email": "user@user.com",
+                "password1": "123",
+                "password2": "%amgf83501767"
+            }
+        )
+        print(response.context["form"])
+        self.assertFalse(response.context["form"].is_valid())
+
+        self.assertEqual(
+            response.context["form"].errors,
+            {"password2": ["The two password fields didn’t match."]}
+        )
