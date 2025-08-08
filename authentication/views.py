137a1a47fbb6eec8ad70003f96d2d0ae11d022ef
RomanSapunGit <sapunroma23@gmail.com>
2025-08-08 17:50:03 +0200

    refactor(register): remove msg attribute and context
    Removed msg because it was unnecessary for showing since built-in errors do their job better.

--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,7 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.views import LoginView
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.urls import reverse
 
 from authentication.forms import SignUpForm, MediaUserLoginForm
@@ -10,7 +10,6 @@ from media.models import MediaUser
 
 
 def register_user(request):
-    msg = None
     success = False
 
     if request.method == "POST":
@@ -23,15 +22,13 @@ def register_user(request):
             login(request, user)
             if user:
                 return HttpResponseRedirect(reverse("media:index"))
-        else:
-            msg = "Form is not valid"
     else:
         form = SignUpForm()
 
     return render(
         request,
         "account/register.html",
-        {"form": form, "msg": msg, "success": success}
+        {"form": form, "success": success}
     )
 
 
